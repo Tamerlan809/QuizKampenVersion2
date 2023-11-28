7dccab0efba1f21ac0f36e7c86b59ce93df1a4ad
Tamerlan Agayev <tamishagayev@gmail.com>
2023-11-28 16:41:48 +0100

    GUI branch

--- src/Client.java
+++ src/Client.java
@@ -13,10 +13,15 @@ public class Client { // Clientarkitektur och GUI
     JFrame frame = new JFrame("");
     JButton buttonStartANewGame = new JButton();
     JButton buttonPlayAFriend = new JButton();
-    JButton buttonAnswerA = new JButton();
-    JButton buttonAnswerB = new JButton();
-    JButton buttonAnswerC = new JButton();
-    JButton buttonAnswerD = new JButton();
+
+    JFrame welcomeFrame = new JFrame();
+
+    JTextField welcomeTextField = new JTextField("Hej och välkommen");
+    JTextField textField = new JTextField("Vilken färg är äpple?");
+    JButton buttonAnswerA = new JButton("A:röd");
+    JButton buttonAnswerB = new JButton("B:grön");
+    JButton buttonAnswerC = new JButton("C;vit");
+    JButton buttonAnswerD = new JButton("D;blå");
 
     /// GUI ............
     private static int port = 43972;
@@ -32,12 +37,27 @@ public class Client { // Clientarkitektur och GUI
                 socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
 
-        startANewGame();
+        welcomeFrame();
 
     }
 
 
     //// GUI.......
+
+    public  void welcomeFrame(){
+        welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        welcomeFrame.setSize(800,650);
+        welcomeFrame.getContentPane().setBackground(new Color(50,50,50));
+        welcomeFrame.setLayout(null);
+        welcomeFrame.setResizable(false);
+        welcomeFrame.setTitle("Quizkampen");
+
+        welcomeTextField.setBounds(0,0,500,100);
+        welcomeTextField.setFont(new Font("MV Boli",Font.BOLD,35));
+
+        welcomeFrame.setVisible(true);
+        welcomeFrame.add(welcomeTextField);
+    }
 
     public void startANewGame(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -47,14 +67,36 @@ public class Client { // Clientarkitektur och GUI
         frame.setResizable(false);
         frame.setTitle("Quizkampen");
 
-        buttonStartANewGame.setBounds(100,400,600,200);
-        buttonStartANewGame.setFont(new Font("MV Boli",Font.BOLD,35));
-        buttonStartANewGame.setFocusable(false);
-        buttonStartANewGame.addActionListener(this);
-        buttonStartANewGame.setText("Start A New Game!");
+        buttonAnswerA.setBounds(0,100,500,100);
+        buttonAnswerA.setFont(new Font("MV Boli",Font.BOLD,35));
+        buttonAnswerA.setFocusable(false);
+
+        buttonAnswerB.setBounds(0,200,500,100);
+        buttonAnswerB.setFont(new Font("MV Boli",Font.BOLD,35));
+        buttonAnswerB.setFocusable(false);
+
+        buttonAnswerC.setBounds(0,300,500,100);
+        buttonAnswerC.setFont(new Font("MV Boli",Font.BOLD,35));
+        buttonAnswerC.setFocusable(false);
+
+        buttonAnswerD.setBounds(0,400,500,100);
+        buttonAnswerD.setFont(new Font("MV Boli",Font.BOLD,35));
+        buttonAnswerD.setFocusable(false);
+
+        textField.setBounds(0,0,500,100);
+        textField.setFont(new Font("MV Boli",Font.BOLD,35));
+
 
         frame.setVisible(true);
-        frame.add(buttonStartANewGame);
+        frame.add(textField);
+        frame.add(buttonAnswerA);
+        frame.add(buttonAnswerB);
+        frame.add(buttonAnswerC);
+        frame.add(buttonAnswerD);
+
+        while(true){
+
+        }
     }
 
     public void playAFriend(){
